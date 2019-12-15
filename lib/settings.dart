206f1dd7a877a0a4996710346b7f5247315c267c
Fabian Graf <fabian.graf@outlook.de>
2019-12-15 12:14:21 +0100

    added settings loader

---
+++ lib/settings.dart
@@ -0,0 +1,30 @@
+import 'dart:async';
+import 'dart:convert';
+import 'dart:io';
+import 'package:http/http.dart' as http;
+import 'dart:async' show Future;
+import 'package:flutter/services.dart' show rootBundle;
+
+class Settings {
+  String theme = "";
+  String settingsJson = "";
+  String parsedSettingsJson = "";
+  String applicationColor = "#000000";
+  String photoprismURL = "";
+
+  Future loadSettings(photoprismURL) async {
+    print("loading settings..");
+
+    http.Response response = await http.get(photoprismURL + '/api/v1/settings');
+
+    final parsed = json.decode(response.body);
+    this.theme = parsed["theme"];
+
+    this.settingsJson = await rootBundle.loadString('assets/themes.json');
+
+    final parsedSettings = json.decode(this.settingsJson);
+
+    final a = parsedSettings[this.theme];
+    this.applicationColor = a["primary"];
+  }
+}
